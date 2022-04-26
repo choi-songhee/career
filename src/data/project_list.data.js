ef56082ec4e6a9bf0790af902b7e11feb9072388
songhee <songhee@dfluid.co.kr>
2022-04-26 23:43:20 +0900

    프로젝트 기술 작업중

--- src/data/project_list.data.js
+++ src/data/project_list.data.js
@@ -2,19 +2,29 @@ export const ProjectList = [
   {
     title: 'LG HOMIN 운영 (프론트서비스퍼블 파트)',
     period: '2022.01 - 2022.04',
-    desc: '',
+    desc: 'LG HOMIN 앱/웹의 기본적인 유지보수와 다양한 기획 요구에 맞는 UI 개선 및 이슈처리를 진행하였습니다' +
+      '\n- 안심비대면 반려견 배상책임 보험 UI 개선 작업' +
+      '\n- 안심비대면 APP용 이벤트 페이지 퍼블리싱 작업' +
+      '\n- 주문서 UI 개선 작업' +
+      '\n- 백오피스 UI 개선 및 이슈사항 처리 작업',
     tech: 'Vue.js / Html / Scss / Javascript / Git / Jira / Zeplin'
   },
   {
-    title: 'SKT 퍼즐 구축',
+    title: 'SK Puzzle 구축',
     period: '2021.10 - 2021.12',
-    desc: '',
+    desc: 'SK open api 플랫폼으로 react.js를 기반으로 하여 모바일, PC, 반응형으로 구축 작업을 진행하였습니다' +
+      '\n- gsap 라이브러리를 사용하여 스크롤 인터랙션 구현' +
+      '\n- react hook을 사용하여 상태관리와 Dom Event 구현' +
+      '\n- 재사용성을 고려하여 컴포넌트 설계 작업 진행',
     tech: 'React.js / Html / Scss / Javascript / Git / Zeplin'
   },
   {
     title: '삼성 SDS 인적자원 Visibility 내부 서비스 구축',
     period: '2021.07 - 2021.09',
-    desc: '',
+    desc: '- Vue.js를 기반으로 한 삼성 SDS 인적자원 내부 서비스 구축 작업을 진행하였습니다' +
+      '\n- Vue 컴포넌트 간 데이터와 이벤트 전달을 사용하여 UI 구성' +
+      '\n- 기획 요구에 따른 기능추가 및 수정작업 진행' +
+      '\n- 웹표준 및 웹 접근성, 크로스브라우징을 고려한 퍼블리싱 작업 진행',
     tech: 'Vue.js / Html / Scss / Javascript / Git / Jira / Jenkins / Zeplin'
   },
   {
@@ -26,22 +36,25 @@ export const ProjectList = [
   {
     title: '삼성카드 약관 개선 프로젝트',
     period: '2021.04 - 2021.05',
-    desc: '',
+    desc: '삼성카드 약관 개선 프로젝트에 퍼블리싱 작업을 진행하였습니다' +
+      '\n- 공통 가이드에 맞추어 공통화에 집중하여 약관 퍼블리싱 작업 진행',
     tech: 'Html / Css / Javascript / Svn'
   },
   {
     title: 'SHANGPREE 다국어 웹사이트 구축',
     period: '2021.01 - 2021.04',
-    desc: '- SHANGPREE 브랜드 소개 웹사이트로 한국어, 영어, 중국어를 지원하는 다국어 웹사이트를 구축하였습니다.',
+    desc: 'SHANGPREE 브랜드 소개 웹사이트로 한국어, 영어, 중국어를 지원하는 다국어 웹사이트를 구축하였습니다.' +
+      '\n- 웹표준 및 웹 접근성, 크로스브라우징을 고려한 반응형 작업 진행' +
+      '\n- yaml 문법을 사용하여 언어별로 데이터 분기 작업 진행',
     tech: 'Nuxt.js / Html / Javascript / Scss / Git / Trello',
     url: 'https://shangpree.co.kr/'
   },
   {
     title: '롯데 ON 운영',
     period: '2020.05 - 2020.12',
-    desc: '- 기본적인 유지보수를 비롯해 다양한 기획 요구에 맞는 새로운 기능과 UI 개편 작업을 하였습니다. ' +
+    desc: '롯데 ON 앱/웹의 기본적인 유지보수를 비롯해 다양한 기획 요구에 맞는 새로운 기능과 UI 개편 작업을 하였습니다. ' +
       '\n- 신규 이벤트 페이지 마크업 및 스크립트 기능 개발' +
-      '\n- 기능개선 및 이슈처리',
+      '\n- 기획 요구에 따른 기능개선 및 이슈처리',
     tech: 'Vue.js / Html / Javascript / Scss / Git / Jira / Zeplin'
   },
   {
@@ -51,15 +64,15 @@ export const ProjectList = [
     tech: 'Vue.js / Html / Javascript / Scss / Git / Jira / Zeplin'
   },
   {
-    title: '비만 365mc 대구지점 웹사이트 구축',
+    title: '비만 365mc 대구지사 웹사이트 구축',
     period: '2019.10 - 2019.11',
-    desc: '비만 365mc 대구지점 소개 웹사이트로 웹표준 및 웹 접근성, 크로스브라우징을 고려한 반응형 퍼블리싱 작업을 진행하였습니다',
+    desc: '비만 365mc 대구지사 소개 웹사이트로 웹표준 및 웹 접근성, 크로스브라우징을 고려한 반응형 퍼블리싱 작업을 진행하였습니다',
     tech: 'Html / Javascript / Scss'
   },
   {
     title: '라이나생명 전성기 영상명함 구축',
     period: '2019.09 - 2019.10',
-    desc: '- 영상명함에 적용될 템플릿을 디자인 가이드에 맞춰 퍼블리싱하는 작업을 진행하였습니다',
+    desc: '라이나 생명 전성기 영상명함에 적용될 템플릿을 디자인 가이드에 맞춰 퍼블리싱하는 작업을 진행하였습니다',
     tech: 'Nuxt.js / Html / Javascript / Css'
   },
   {
